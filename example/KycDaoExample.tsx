4c67a58f473436569a0724c99b24e09b9f2a1cf9
Sandoche Adittane <sandoche@protonmail.com>
2022-07-20 15:27:53 +0100

    Work in progress example

--- example/KycDaoExample.tsx
+++ example/KycDaoExample.tsx
@@ -1,8 +1,138 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useKycDao } from '../.';
 
 const KycDaoExample = () => {
-  return <div>Hello world</div>;
+  const kycDao = useKycDao();
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isKycCompleted, setIsKycCompleted] = React.useState(false);
+  const [isKycValid, setIsKycValid] = React.useState(false);
+  const counter = React.useRef(0);
+
+  // A function to open the modal and load the verification Data
+  const runKycModal = React.useCallback(async () => {
+    if (!kycDao) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { VerificationTypes } = await import('@kycdao/kycdao-sdk');
+
+    const verificationData = {
+      email: 'fake@email.com',
+      isEmailConfirmed: true,
+      taxResidency: 'FR',
+      isLegalEntity: false,
+      verificationType: VerificationTypes.KYC,
+      termsAccepted: true,
+    };
+
+    const options = {
+      personaOptions: {
+        onCancel: () => {
+          setIsLoading(false);
+        },
+        onComplete: async () => {
+          setIsKycCompleted(true);
+          setIsLoading(false);
+        },
+        onError: () => {
+          setIsLoading(false);
+        },
+      },
+    };
+
+    await kycDao.registerOrLogin();
+    const { KYC } = await kycDao.checkVerificationStatus();
+
+    if (!KYC) {
+      kycDao.startVerification(verificationData, options);
+    }
+  }, [kycDao]);
+
+  // Run the KYC modal if connected to the smartcontract
+  React.useEffect(() => {
+    const startKycAction = () => {
+      if (!isKycValid && kycDao?.connectedWallet && counter.current === 0) {
+        // to avoid multiple calls to registerOrLogin
+        counter.current += 1;
+
+        setIsLoading(true);
+        runKycModal();
+      }
+    };
+
+    startKycAction();
+  }, [isKycValid, kycDao?.connectedWallet, runKycModal]);
+
+  // Start minting the tokens if the KYC is completed
+  const mintSbt = async () => {
+    if (!kycDao) {
+      return;
+    }
+
+    setIsLoading(true);
+    await kycDao.startMinting({
+      disclaimerAccepted: true,
+    });
+    setIsLoading(false);
+  };
+
+  // this example uses useQuery to make it simpler to poll the api to get the KYC status
+  const { isLoading: validationLoading } = useQuery(
+    ['validate-kyc'],
+    () => {
+      return kycDao && kycDao.checkVerificationStatus();
+    },
+    {
+      refetchOnWindowFocus: true,
+      refetchInterval: 2000,
+      enabled: kycDao ? kycDao.walletConnected : false,
+      onSuccess: data => {
+        if (data?.KYC === true) {
+          setIsKycValid(true);
+        }
+      },
+    }
+  );
+
+  // Start the connection to the smartcontract
+  const connect = async () => {
+    if (!kycDao) {
+      return;
+    }
+
+    counter.current = 0;
+
+    setIsLoading(true);
+    await kycDao.connectWallet('Near');
+  };
+
+  const waitingForValidation = isKycCompleted && !isKycValid;
+  const isLoadingOrWaitingForValidation =
+    isLoading || validationLoading || waitingForValidation;
+
+  return (
+    <div>
+      {isKycValid ? (
+        <>
+          <button onClick={mintSbt} disabled={isLoadingOrWaitingForValidation}>
+            {isLoadingOrWaitingForValidation ? 'Loading...' : 'Mint SBT'}
+          </button>
+        </>
+      ) : (
+        <>
+          <button onClick={connect} disabled={isLoadingOrWaitingForValidation}>
+            {isLoadingOrWaitingForValidation
+              ? 'Loading...'
+              : 'Connect & start Kyc'}
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default KycDaoExample;
--- example/index.tsx
+++ example/index.tsx
@@ -1,17 +1,23 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// This library is not required but used to make it simpler
 import { KycDaoProvider } from '../.';
 import KycDaoExample from './KycDaoExample';
 
 const App = () => {
+  const [queryClient] = React.useState(() => new QueryClient()); // This library is not required but used to make it simpler
   const nearNetworkEnv = 'testnet';
 
   return (
     <div>
-      <KycDaoProvider networkId={nearNetworkEnv} config={{}}>
-        <KycDaoExample />
-      </KycDaoProvider>
+      {/* QueryClientProvider is not required but used to make it simpler */}
+      <QueryClientProvider client={queryClient}>
+        <KycDaoProvider networkId={nearNetworkEnv} config={{}}>
+          <KycDaoExample />
+        </KycDaoProvider>
+      </QueryClientProvider>
     </div>
   );
 };
